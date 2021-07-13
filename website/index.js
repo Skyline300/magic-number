1522023e125c89dfb2ebf49bf1eaf2f75f49373e
Skyline300 <79264371+Skyline300@users.noreply.github.com>
2021-07-13 13:13:25 +0800

    Update index.js

--- website/index.js
+++ website/index.js
@@ -2,3 +2,9 @@ window.addEventListener('DOMContentLoaded', (event) => {
     alert("Dom Loaded")
     console.log('DOM fully loaded and parsed');
 });
+
+function simpleAlert(){
+    alert("Button Pressed")
+}
+const createSessionButton = document.getElementById('create-session-button')
+createSessionButton.addEventListener("click",()=>{simpleAlert()})

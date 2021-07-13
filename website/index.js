6aa9d21a4702dcba7a39cb4ea5235b83dffed99b
Skyline300 <79264371+Skyline300@users.noreply.github.com>
2021-07-13 14:24:28 +0800

    uhhhhhhhhhhhhh

--- website/index.js
+++ website/index.js
@@ -1,10 +1,16 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     alert("Dom Loaded")
     console.log('DOM fully loaded and parsed');
-});
+    function simpleAlert() {
+        alert("Button Pressed")
+    }
+    const createSessionButton = document.getElementById('create-session-button')
+    createSessionButton.addEventListener("click", () => {
+        simpleAlert()
+        fetch('http://localhost:8000/', {
+            method: 'PUT'
+        }).then(response => response.json())
+            .then(data => console.log(data));
+    })
 
-function simpleAlert(){
-    alert("Button Pressed")
-}
-const createSessionButton = document.getElementById('create-session-button')
-createSessionButton.addEventListener("click",()=>{simpleAlert()})
+});

b96deca31d3a1562744c731bfce4dd4f97dfb43e
Skyline300 <79264371+Skyline300@users.noreply.github.com>
2021-07-13 14:51:56 +0800

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,14 @@
 const express = require('express')
+const app = express()
 const cors = require('cors')
 const nanoid = require('nanoid')
+
+app.use(cors())
+
+app.post('/',function(req,res,next){
+    res.send("Creating New Session")
+})
+
+app.listen(8000,function () {
+    console.log('Listening on port 8000')
+})
